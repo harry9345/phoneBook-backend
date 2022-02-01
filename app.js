434298658e37f1080f7a16a48ee1066b3664ae10
Hossein Movahed <hossein.movahed@monese.com>
2022-02-01 09:42:54 +0200

    Controlling the state of the database

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const config = require("./utils/config");
-const express = require("express");
-require("express-async-errors");
+const config = require('./utils/config');
+const express = require('express');
+require('express-async-errors');
 const app = express();
-const cors = require("cors");
-const notesRouter = require("./controllers/note");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
-const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-logger.info("connecting to : ", config.MONGODB_URI);
+const cors = require('cors');
+const notesRouter = require('./controllers/note');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+logger.info('connecting to : ', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info("connected to mongo DB0");
+    logger.info('connected to mongo DB0');
   })
   .catch((error) => {
-    logger.error("error connecting to mongo DB O: ", error.message);
+    logger.error('error connecting to mongo DB O: ', error.message);
   });
 
 app.use(cors());
-app.use(express.static("build"));
+app.use(express.static('build'));
 app.use(express.json());
 
 app.use(middleware.requestLogger);
 
-app.use("/app/notes", notesRouter);
-app.use("/app/users", usersRouter);
-app.use("/app/login", loginRouter);
+app.use('/app/notes', notesRouter);
+app.use('/app/users', usersRouter);
+app.use('/app/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
